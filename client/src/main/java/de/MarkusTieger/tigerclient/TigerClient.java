e3611a7623ca313812e23b9b0743a8f556b447f8
MarkusTieger <markustieger@gmail.com>
2022-09-25 12:40:57 +0200

    Fixed Message in "en_us"

--- client/src/main/java/de/MarkusTieger/tigerclient/TigerClient.java
+++ client/src/main/java/de/MarkusTieger/tigerclient/TigerClient.java
@@ -99,8 +99,8 @@ public class TigerClient extends Client
 	private static final Logger APACHE_LOGGER = LogManager.getLogger();
 
 	public static final String versionType = "Beta";
-	public static final String clVersion = "2.10.0";
-	public static final String build = "0023";
+	public static final String clVersion = "2.10.1";
+	public static final String build = "0024";
 	public static final String version_format = "%s-%s";
 	private static final String version = String.format(version_format, clVersion, build);
 	private static final int versionNumber = 2;
